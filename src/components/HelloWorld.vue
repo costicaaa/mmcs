6e7e9cac080e43d0e1ca37fd62b014b0e56f3e44
costin <costin.costache@digitalkrikitz.com>
2019-10-30 13:31:45 +0200

    matrix init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,81 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div class="container">
+        <div class="well well-lg">
+            <form id="inputField" role="form">
+
+                <input type="text" name="field00" size="3">
+                <input type="text" name="field00" size="3" value="Team 1">
+                <input type="text" name="field00" size="3" value="Team 2">
+                <input type="text" name="field00" size="3" value="Team 3">
+                <input type="text" name="field00" size="3" value="Team 4">
+
+                <br>
+                <input type="text" name="field00" size="3" value="Team 1">
+                <input type="text" name="field01" size="3" value="      -">
+                <input type="text" name="field00" size="3">
+                <input type="text" name="field02" size="3">
+                <input type="text" name="field03" size="3">
+
+                <br>
+                <input type="text" name="field10" size="3" value="Team 2">
+                <input type="text" name="field10" size="3">
+                <input type="text" name="field01" size="3" value="      -">
+                <input type="text" name="field12" size="3">
+                <input type="text" name="field13" size="3">
+
+                <br>
+                <input type="text" name="field10" size="3" value="Team 3">
+                <input type="text" name="field20" size="3">
+                <input type="text" name="field21" size="3">
+                <input type="text" name="field01" size="3" value="      -">
+                <input type="text" name="field23" size="3">
+
+                <br>
+                <input type="text" name="field10" size="3" value="Team 4">
+                <input type="text" name="field20" size="3">
+                <input type="text" name="field21" size="3">
+                <input type="text" name="field22" size="3">
+                <input type="text" name="field01" size="3" value="      -">
+                <br>
+
+                <br>
+                <br>
+                <br>
+                <br>
+                <input type="submit" onclick="calcJacobian()" value="calculate" name="calculate" class="btn btn-info">
+            </form>
+
+        </div>
+
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        name: 'HelloWorld',
+        props: {
+            msg: String
+        }
+    }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
+
+    a {
+        color: #42b983;
+    }
 </style>
